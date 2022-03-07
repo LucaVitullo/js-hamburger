344b7c77b103e6a25c9d424abbb4f20d91db3d09
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-03-07 16:09:00 +0100

    hamburgerMenuClose added

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 console.log('js ok')
 
 const hamburgerMenu = document.querySelector('.hamburger-menu');
-
-const hamburgerMenuOpen = document.querySelector('.header-right a i');
+const hamburgerMenuOpen = document.querySelector('.fa-bars');
+const hamburgerMenuClose = document.querySelector('.fa-times');
 
 hamburgerMenuOpen.addEventListener('click', function () {
     console.log('HamburgerMenuOpen')
     hamburgerMenu.style.display = 'block'
+})
+
+hamburgerMenuClose.addEventListener('click', function () {
+    console.log('HamburgerMenuClose')
+    hamburgerMenu.style.display = 'none'
 })
