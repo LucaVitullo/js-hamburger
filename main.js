5db7c92e12ab0952ea23bb61b9dddfbfe73b233c
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-03-07 17:43:16 +0100

    second option completed

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const hamburgerMenu = document.querySelector('.hamburger-menu');
 const hamburgerMenuOpen = document.querySelector('.fa-bars');
 const hamburgerMenuClose = document.querySelector('.fa-times');
 
+/////FIRST OPTION/////
+
+/*
 hamburgerMenuOpen.addEventListener('click', function () {
     console.log('HamburgerMenuOpen')
     hamburgerMenu.style.display = 'block'
@@ -13,3 +16,17 @@ hamburgerMenuClose.addEventListener('click', function () {
     console.log('HamburgerMenuClose')
     hamburgerMenu.style.display = 'none'
 })
+
+*/
+
+
+/////SECOND OPTION - BONUS//////
+
+hamburgerMenuOpen.addEventListener('click',function () {
+    console.log('HamburgerMenuOpen')
+    hamburgerMenu.classList.add('active');
+});
+hamburgerMenuClose.addEventListener('click',function () {
+    console.log('HamburgerMenuClose')
+    hamburgerMenu.classList.remove('active');
+});
