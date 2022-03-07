3ffca1e0d2304adfc79eb16862a60dbe135f3f34
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-03-07 15:30:28 +0100

    hamburgerMenuOpen added

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 console.log('js ok')
 
-const element = document.querySelector('hamburger-menu');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
 
+const hamburgerMenuOpen = document.querySelector('.header-right a i');
+
+hamburgerMenuOpen.addEventListener('click', function () {
+    console.log('HamburgerMenuOpen')
+    hamburgerMenu.style.display = 'block'
+})
